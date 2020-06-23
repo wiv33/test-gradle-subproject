631467f9f8cd4bc9261d07896a97ddab0574a5a1
PSK <wiv821253@gmail.com>
2020-06-23 17:30:24 +0900

    add annotationProcessor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    `maven-publish`
+//    `maven-publish`
     id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("com.palantir.docker") version "0.22.1"
@@ -11,6 +11,12 @@ version = "1.0.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_13
 
 extra["springCloudVersion"] = "Hoxton.SR5"
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
